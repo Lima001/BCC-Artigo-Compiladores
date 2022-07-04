702e58c96a293c2b3c6b356aa35c0663d64555fa
Lima001 <limaedugabriel@gmail.com>
2022-07-04 09:10:06 -0300

    Adição de Arquivos

---
+++ scriptTempoCompilacao.sh
@@ -0,0 +1,2 @@
+bash ./Scripts/compilarMerge.sh 2> Medições/compilar_merge.txt
+bash ./Scripts/compilarBubble.sh 2> Medições/compilar_bubble.txt
---
+++ scriptTempoExecucao.sh
@@ -0,0 +1,2 @@
+bash ./Scripts/execucaoMerge.sh 2> Medições/tempo_merge.txt
+bash ./Scripts/execucaoBubble.sh 2> Medições/tempo_bubble.txt
